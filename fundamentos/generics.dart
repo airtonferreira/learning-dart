497b7d211a336a050d9801d4b85974f2385b3ca6
airton.felix <airton.felix@dcx.ufpb.br>
2020-05-17 00:34:36 +0000

    generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,19 @@
+main() {
+// List<String> somethings = [
+//     'macbook',
+//     true,
+//     153,
+//     13.0,
+
+//   ];
+//**Esse array está errado, pq ela só vai aceitar elementos do tipo String. */
+
+  List<Object> somethings = [
+    'macbook',
+    true,
+    153,
+    13.1,
+  ];
+
+/**Para usar qualquer tipo numa lista, vc pode passar Object ou usar o tipo var para sua variável */
+}
