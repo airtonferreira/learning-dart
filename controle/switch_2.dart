4250eb12e7b55ce13931de7f99ff6d231a740cf6
airton.felix <airton.felix@dcx.ufpb.br>
2020-05-19 23:38:06 +0000

    switch case continue

---
+++ controle/switch_2.dart
@@ -0,0 +1,19 @@
+main() {
+  for (int a = 0; a < 10; a++) {
+    if (a == 6) {
+      break;
+    }
+    print(a);
+  }
+
+  print('Dps do FOR 01');
+
+  for (int a = 0; a < 10; a++) {
+    if (a % 2 == 0) {
+      continue;
+    }
+    print(a);
+  }
+
+  print('Dps do FOR 02');
+}
