725295f60002489a78a07a32bf9ac96e7e0d24ca
airton.felix <airton.felix@dcx.ufpb.br>
2020-06-05 23:09:38 -0300

    Return Functions

---
+++ funcao/function_return.dart
@@ -0,0 +1,17 @@
+Function(int) somaParcial(int a) {
+  int z = 0;
+
+  return (int b) {
+    return a + b + z;
+  };
+}
+
+main() {
+  print(somaParcial(2)(10));
+
+  var somaComDez = somaParcial(10);
+
+  print(somaComDez(3));
+  print(somaComDez(7));
+  print(somaComDez(20));
+}
